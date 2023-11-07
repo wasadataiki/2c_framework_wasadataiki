c8ef4a5a6f9e1ef022b1619d4fc5d7d01c5905da
wasadataiki <taiki,.wasada@gmail.com>
2023-11-07 12:10:57 +0900

    test

---
+++ MyGitProject2c/src/main/java/com/example/demo/MyGitProject2cApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitProject2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitProject2cApplication.class, args);
+	}
+
+}
